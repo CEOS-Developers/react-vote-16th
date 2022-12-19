868f086405c6fc6ee036761dc16ead95832ef392
jieunOh <0909oje@naver.com>
2022-12-19 21:37:19 +0900

    feat : recoil 설치

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,12 +1,12 @@
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import React from "react";
-import { RecoilRoot } from "recoil";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import React from 'react';
+import { RecoilRoot } from 'recoil';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
