c354e2015edc1a0f0ab97883290701dea54e61c7
ch9eri <limch9eri@gmail.com>
2022-12-20 16:37:02 +0900

    [FEAT] 로그아웃 구현

--- vote/src/Components/Header.tsx
+++ vote/src/Components/Header.tsx
@@ -1,20 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { isSignInState } from '../state/state';
+import axios from 'axios';
 
 const Header = () => {
+  const [isSignIn, setIsSignIn] = useRecoilState<boolean>(isSignInState);
+  axios.defaults.baseURL = 'http://3.38.123.37';
+  const navigate = useNavigate();
+
+  const signOutAPI = async () => {
+    await axios
+      .post('/api/auth/logout/')
+      .then((response) => {
+        console.log(response.data);
+        alert('로그아웃 성공! 로그인 화면으로 이동합니다.');
+        navigate('/'); //홈으로 가서 로그인하게 만들기
+      })
+      .catch(function (error) {
+        console.log(error);
+        alert('로그아웃 실패');
+      });
+  };
+
   return (
     <div>
-        <Link to='/home'>
-      <Logo src={require('../Img/ceos.webp')} alt="CEOS" />
-      </Link>
+      {isSignIn === true ? (
+        <Link to="/home">
+          <Logo src={require('../Img/ceos.webp')} alt="CEOS" />
+        </Link>
+      ) : (
+        <Link to="/">
+          <Logo src={require('../Img/ceos.webp')} alt="CEOS" />
+        </Link>
+      )}
+
+      {isSignIn === true ? <SignBtn onClick={signOutAPI}>로그아웃</SignBtn> : <></>}
     </div>
   );
 };
 
 const Logo = styled.img`
-  width: 165px;
-  height: 60px;
+  width: 130px;
+  height: 50px;
+  margin-bottom: 10px;
 `;
+
+const SignBtn = styled.button``;
 
 export default Header;
--- vote/src/Router.tsx
+++ vote/src/Router.tsx
@@ -4,13 +4,14 @@ import SignInPage from './Page/SignInPage';
 import SignUpPage from './Page/SignUpPage';
 import Voting from './Page/voting';
 import VoteResult from './Page/voteResult';
+import Header from './Components/Header';
 
 const Router = () => {
   return (
     <BrowserRouter>
     {/* 일단 보류 */}
     {/* Header는 로그인 상태일 때만 표시 */}
-      {/* <Header /> */}
+      <Header />
       <Routes>
         {/* 첫 화면은 로그인(/): SignInPage */}
         {/* 로그인 후 메인화면(/home): HomePage*/}
