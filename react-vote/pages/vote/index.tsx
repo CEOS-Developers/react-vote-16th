2ecb4ad4f13e29bea5cac03da4db7f7b470cd16e
hamo-o <yheel369@gmail.com>
2022-12-21 22:06:15 +0900

    style: vote 페이지 UI 변경

--- react-vote/pages/vote/index.tsx
+++ react-vote/pages/vote/index.tsx
@@ -34,67 +34,73 @@ const Vote = () => {
 
   return (
     <div className="container">
-      <div className="frontend">
-        <Link
-          href={{
-            pathname: `/vote/[team]`,
-            query: {
-              vote_list: JSON.stringify(
-                cand?.filter((cand) => cand.position === team)
-              ),
-            },
-          }}
-          as={`/vote/${team}`}
-        >
-          FE 투표하기
-        </Link>
-        <Link
-          href={{
-            pathname: `/result/FE`,
-            query: {
-              vote_list: JSON.stringify(
-                cand?.filter((cand) => cand.position === 'FE')
-              ),
-            },
-          }}
-          as={`/result/FE`}
-        >
-          FE 결과보기
-        </Link>
-      </div>
-      <div className="backend">
-        <Link
-          href={{
-            pathname: `/vote/[team]`,
-            query: {
-              vote_list: JSON.stringify(
-                cand?.filter((cand) => cand.position === team)
-              ),
-            },
-          }}
-          as={`/vote/${team}`}
-        >
-          BE 투표하기
-        </Link>
-        <Link
-          href={{
-            pathname: `/result/BE`,
-            query: {
-              vote_list: JSON.stringify(
-                cand?.filter((cand) => cand.position === 'BE')
-              ),
-            },
-          }}
-          as={`/result/BE`}
-        >
-          BE 결과보기
-        </Link>
-      </div>
+      <Link
+        href={{
+          pathname: `/vote/[team]`,
+          query: {
+            vote_list: JSON.stringify(
+              cand?.filter((cand) => cand.position === team)
+            ),
+          },
+        }}
+        as={`/vote/${team}`}
+      >
+        <div className="vote-btn">FE 투표하기</div>
+      </Link>
+      <Link
+        href={{
+          pathname: `/result/FE`,
+          query: {
+            vote_list: JSON.stringify(
+              cand?.filter((cand) => cand.position === 'FE')
+            ),
+          },
+        }}
+        as={`/result/FE`}
+      >
+        <div className="vote-btn">FE 결과보기</div>
+      </Link>
+
+      <Link
+        href={{
+          pathname: `/vote/[team]`,
+          query: {
+            vote_list: JSON.stringify(
+              cand?.filter((cand) => cand.position === team)
+            ),
+          },
+        }}
+        as={`/vote/${team}`}
+      >
+        <div className="vote-btn">BE 투표하기</div>
+      </Link>
+      <Link
+        href={{
+          pathname: `/result/BE`,
+          query: {
+            vote_list: JSON.stringify(
+              cand?.filter((cand) => cand.position === 'BE')
+            ),
+          },
+        }}
+        as={`/result/BE`}
+      >
+        <div className="vote-btn">BE 결과보기</div>
+      </Link>
+
       <style jsx>{`
+        .vote-btn {
+          width: 10em;
+          height: 10rem;
+          padding: 2rem;
+          border: 1px solid white;
+          border-radius: 1rem;
+        }
+
         .container {
-          display: flex;
-          flex-direction: column;
-          gap: 1rem;
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          gap: 1rem 1rem;
         }
       `}</style>
     </div>
